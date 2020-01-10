ec4ed203b8b13ad1c79535e8fb869a789c5cee2c
Chris <45579102+ChrisJol@users.noreply.github.com>
2020-01-10 13:44:33 -0800

    javascript optimization test

--- javascript/animate.js
+++ javascript/animate.js
@@ -1,21 +1,28 @@
-let addSpin = () => {
-    document.querySelector(".bloom").classList.add("spin")
-}
-
-gsap.timeline({defaults: {duration: 4, delay: .1, ease: "power4.inOut"}})
-.from('.view__splash--letter', {
-    opacity: 0,
-    y: "random(-100, 100)",
-    stagger: {
-        amount: 1,
-        from: "center"
-    }
-}, 0)
-.to('.one', { xPercent: -100, scale: 2, opacity: .5 }, 0)
-.to('.two', { xPercent: -70, yPercent: -70, scale: 2, opacity: .5 }, 0)
-.to('.three', { yPercent: -100, scale: 2, opacity: .5 }, 0)
-.to('.four', { xPercent: 70, yPercent: -70, scale: 2, opacity: .5}, 0)
-.to('.five', { xPercent: 100, scale: 2, opacity: .5 }, 0)
-.to('.six', { xPercent: 70, yPercent: 70, scale: 2, opacity: .5 }, 0)
-.to('.seven', { yPercent: 100, scale: 2, opacity: .5 }, 0)
-.to('.eight', { xPercent: -70, yPercent: 70, scale: 2, opacity: .5 }, 0)
+$.when(
+    $.getScript("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"),
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.0.4/gsap.min.js"),
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.0.4/ScrollToPlugin.min.js"),
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"),
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"),
+    // $.getScript("javascript/script.js"),
+    // $.getScript("javascript/carousel.js")
+)
+.done( function(){
+    gsap.timeline({defaults: {duration: 4, delay: .1, ease: "power4.inOut"}})
+    .from('.view__splash--letter', {
+        opacity: 0,
+        y: "random(-100, 100)",
+        stagger: {
+            amount: 1,
+            from: "center"
+        }
+    }, 0)
+    .to('.one', { xPercent: -100, scale: 2, opacity: .5 }, 0)
+    .to('.two', { xPercent: -70, yPercent: -70, scale: 2, opacity: .5 }, 0)
+    .to('.three', { yPercent: -100, scale: 2, opacity: .5 }, 0)
+    .to('.four', { xPercent: 70, yPercent: -70, scale: 2, opacity: .5}, 0)
+    .to('.five', { xPercent: 100, scale: 2, opacity: .5 }, 0)
+    .to('.six', { xPercent: 70, yPercent: 70, scale: 2, opacity: .5 }, 0)
+    .to('.seven', { yPercent: 100, scale: 2, opacity: .5 }, 0)
+    .to('.eight', { xPercent: -70, yPercent: 70, scale: 2, opacity: .5 }, 0)
+})
