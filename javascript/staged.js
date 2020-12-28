149c166b11f7df3d684e83db20fbb9b99b550f8d
Chris Jol <jol.chris2@gmail.com>
2020-12-28 12:48:14 -0800

    moved dependency import to html

--- javascript/staged.js
+++ javascript/staged.js
@@ -1,57 +1,10 @@
 let navDots = document.querySelectorAll(".nav__dot")
     controller = new ScrollMagic.Controller()
-    scripts = [
-        "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.0.4/gsap.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.0.4/ScrollToPlugin.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js",
-        // "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js",
-        // "javascript/scroll.js",
-        // "javascript/tween.js",
-        // "javascript/carousel.js"
-    ]
-
-let loadScripts = (scripts) => {
-    var deffered = $.Deferred();
-
-    let loadScript = (i) => {
-        if ( i < scripts.length ){
-            $.getScript(scripts[i], () => {
-                loadScript( i + 1)
-            })
-        } else {
-            deffered.resolve()
-        }
-    }
-
-    loadScript(0)
-
-    return deffered
-}
 
 let setVh = () => {
     let vh = window.innerHeight
     document.documentElement.style.setProperty('--vh', `${vh}px`)
 }
-
-//setup
-setScrollPosition()
-window.addEventListener("scroll", function(){ setScrollPosition() })
-setVh()
-window.addEventListener('resize', setVh)
-loadScripts(scripts)
-.done( () => {
-    gsap.from('.view__splash--letter', {
-        duration: 2,
-        ease: "power4.inOut",
-        opacity: 0,
-        y: "random(-100, 100)",
-        stagger: {
-            amount: 1,
-            from: "center"
-        }
-    }, 0)
-})
 
 //function definitions
 let animateDot = (selector) => {
@@ -92,6 +45,25 @@ let setScrollPosition = () =>{
         animateDot(".splash")
     }
 }
+
+//setup
+setScrollPosition()
+window.addEventListener("scroll", function(){ setScrollPosition() })
+setVh()
+window.addEventListener('resize', setVh)
+
+
+//initial animations
+gsap.from('.view__splash--letter', {
+    duration: 2,
+    ease: "power4.inOut",
+    opacity: 0,
+    y: "random(-100, 100)",
+    stagger: {
+        amount: 1,
+        from: "center"
+    }
+}, 0)
 
 //ABOUT page tween and scroll scene
 let aboutTween = gsap.timeline()
